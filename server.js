a9bb63eab70ae6ebba59947e9217736f3221afe5
Ben Kelly <ben@wanderview.com>
2015-10-11 16:16:40 -0400

    remove files directory to support github pages

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ var compression = require('compression');
 var app = express();
 
 app.use(compression({ filter: function(req, res) { return true; } }));
-app.use(serveStatic('files', { index: true }));
+app.use(serveStatic('.', { index: true }));
 
 app.listen(3000);
